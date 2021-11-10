6a97d44e3ed2b4b34f8eed4464f98b5e6f805e0d
nithish <nithishgoudb.gmail.com>
2021-11-10 22:06:32 +0530

    second commit

---
+++ Sample.java
@@ -0,0 +1,9 @@
+public class Samplejavaapp{
+
+  public void mul(){
+  system.out.println("hi");
+
+}
+}
+
+   
---
+++ Test.java
@@ -0,0 +1,7 @@
+public Class Testjava{
+
+public static void main(String args[]){
+
+}
+
+}
